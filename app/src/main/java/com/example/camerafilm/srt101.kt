20bb0d0229622f3fa977b6035d843fa7df206dc3
teerawatgithub <59160069@go.buu.ac.th>
2019-11-21 02:00:59 +0700

    Toast

--- app/src/main/java/com/example/camerafilm/srt101.kt
+++ app/src/main/java/com/example/camerafilm/srt101.kt
@@ -1,11 +1,11 @@
 package com.example.camerafilm
 
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.*
+import android.view.Menu
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 
 /**
  * A simple [Fragment] subclass.
@@ -17,8 +17,33 @@ class srt101 : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
+        setHasOptionsMenu(true)
         return inflater.inflate(R.layout.fragment_srt101, container, false)
     }
 
+    private fun getShareIntent() : Intent {
+        val shareIntent = Intent(Intent.ACTION_SEND)
+        shareIntent.setType("text/plain")
+            .putExtra(Intent.EXTRA_TEXT, getString(R.string.share_success_text))
+        return shareIntent
+    }
+    private fun shareSuccess() {
+        startActivity(getShareIntent()) //
+    }
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater.inflate(R.menu.share_menu, menu)
+        // check if the activity resolves
+        if (null == getShareIntent().resolveActivity(activity!!.packageManager)) {
+            // hide the menu item if it doesn't resolve
+            menu.findItem(R.id.share)?.setVisible(false)
+        }
+    }
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.share -> shareSuccess()
+        }
+        return super.onOptionsItemSelected(item)
+    }
 
 }
